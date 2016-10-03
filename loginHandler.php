836be013c2eae6710455143eefc59633f997ca88
bfang <benjamin.kevin.fang@gmail.com>
2016-10-03 11:24:27 -0500

    display results of curl call in loginHandler.php

--- loginHandler.php
+++ loginHandler.php
@@ -14,15 +14,18 @@ curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($ch, CURLOPT_COOKIEJAR, "tmp\cookieFile.txt");
 curl_setopt($ch, CURLOPT_URL,"https://internal.softlayer.com");
 curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_POSTFIELDS, "data[User][username]=$username&data[User][password]=$password&data[User][externalToken]=$externalToken");
 
 ob_start();      // prevent any output
-curl_exec ($ch); // execute the curl command
+$result = curl_exec ($ch); // execute the curl command
 ob_end_clean();  // stop preventing output
+
+var_dump($result);
 
 curl_close ($ch);
 
-echo "logged in"
+//echo "logged in"
 
 //exec('php main.php');
 ?>
